73f32e6a92d510ad6fa2307d0265990292205771
Shailyn Pillay <shailynpillay@gmail.com>
2019-08-20 14:08:54 +1200

    First Kermit

---
+++ src/main/java/nz/ac/eit/amazingtranslator69/App.java
@@ -0,0 +1,13 @@
+package nz.ac.eit.amazingtranslator69;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "https://github.com/sauq32/congenial-happiness.git" );
+    }
+}
